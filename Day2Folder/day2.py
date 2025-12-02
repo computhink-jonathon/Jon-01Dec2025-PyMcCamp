ad1262734dff34710c916e1aba3912352ba386aa
computhink-jonathon <JONATHON.LEE@COMPUTHINK.COM.SG>
2025-12-02 08:48:52 +0000

    Dec 2, 2025, 8:48 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,6 +1,45 @@
 # Write all your codes for Day 2 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day2")
+# print("hello from day2")
+
+# for count in range(2):
+#     print("hello")
+# print("world")
+
+# name = "Javen".upper()
+# for letter in name:
+#     print("Give me a " + name + "!")
+#     print("Give me a " + letter + "!")
+
+# print("Who is the best??!?")
+# print(name+"!!")
+
+# for count in range(10,0,1):
+#     print(count)
+
+# for count in range(50,0,-1):
+#     print(count)
+
+# for count in range(10):
+#     print(count)
+
+
+# for count in range(100,-1,-2):
+#     print(count)
+
+# for i in range(10):
+#     print(i)
+
+# for j in range(2):
+#     print(j)
+
+for char in "today":
+    print(char)
+
+name = "ethan"
+
+for s in name:
+    print(s)
 
 ########################################################################
 # Task 1:
