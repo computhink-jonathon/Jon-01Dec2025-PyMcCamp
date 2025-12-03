397faaf2885bb1b9caae850c30266fe0359b4e45
computhink-jonathon <JONATHON.LEE@COMPUTHINK.COM.SG>
2025-12-03 07:44:57 +0000

    Dec 3, 2025, 7:44 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,7 +1,15 @@
 # Write all your codes for Day 3 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# print("hello from day3")
+# myName = input("What is your name?")
+# print("Oh your name is " + myName)
 
+# myNum = int(input("Give me a number"))
+# print("Oh your number is " + str(myNum))
+# myNum2 = input ("Give me another number")
+# sum = myNum + int(myNum2)
+# print("The total is " + str(sum))
+# # all input are in strings
 ########################################################################
 # Task 1:
 
@@ -9,17 +17,27 @@ print("hello from day3")
 
 ########################################################################
 # Task 2:
-
+name = "Name"
+num_pens = 20
+print(name + " brought " + str(num_pens) + " pens.")
 
 
 ########################################################################
 # Task 3:
-
+num1 = int(input("first number is ?"))
+num2 = int(input("second number is ?"))
+sum = num1 + num2
+# num 1= 10 num2 = 3
+# 10 + 3 = 13
+ 
 
 
 ########################################################################
 # Task 4:
-
+price = 8
+amt_of_item = int(input("How much item do you need?"))
+total_cost = price * amt_of_item
+print("The total cost of item is " + str(total_cost))
 
 
 ########################################################################
@@ -30,7 +48,7 @@ print("hello from day3")
 ########################################################################
 # Task 6:
 
-
+for count in range():
 
 ########################################################################
 # Task 7:
