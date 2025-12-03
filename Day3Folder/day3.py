6f8c062818fe88a55f561027f902f8c09bf30e41
computhink-jonathon <JONATHON.LEE@COMPUTHINK.COM.SG>
2025-12-03 08:06:21 +0000

    Dec 3, 2025, 8:06 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -75,7 +75,10 @@ result = input("What is " + str(num1) + " + " + str(num2) + " ?")
 # the program will print out “You are smart.”
 # If the student answers wrongly, 
 # the program will print out “Go and see the principal.”
-if result == sum
+if result == sum:
+    print("You are smart.")
+else:
+    print()
 
 
 ########################################################################
